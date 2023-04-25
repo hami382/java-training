9345cd2771f3cc0fecb867972098699d6e4ab37c
Hami Moghimy <hamimoghimy1378@gmail.com>
2023-04-25 20:17:36 +0330

    First Lesson

---
+++ Main1.java
@@ -0,0 +1,33 @@
+public class Main1 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+
+        int x = 250;
+        double y = 3.14;
+        boolean z = true;
+        char symbol = '@';
+        String myName = "Hami";
+        String myLastName = "Moghimy";
+
+        System.out.println("My number is: " + x);
+        System.out.println("Number Pi: " + y);
+        System.out.println("Are you student? " + z);
+        System.out.println("My Gmail account is: hamimoghimy1378" + symbol + "gmail.com");
+        System.out.println("My name is: " + myName);
+        System.out.println("My lastname is: " + myLastName);
+
+        String T = "Hami";
+        String M = "Adib";
+        String J;
+
+        J = T;
+        T = M;
+        M = J;
+
+        System.out.println("T: " + T);
+        System.out.println("M: " + M);
+
+
+    }
+}
